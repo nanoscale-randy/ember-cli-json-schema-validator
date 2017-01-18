fbcff60a221eacaea78b4cf3a591ece4a872eff5
Randall Morey <rmorey@anypresence.com>
2017-01-18 11:04:45 -0500

    fixed syntax

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-json-schema-validator'
+  name: 'ember-cli-json-schema-validator',
   included: function (app) {
     this._super.included.apply(this, arguments);
     app.import(app.bowerDirectory + '/jjv/lib/jjv.js');

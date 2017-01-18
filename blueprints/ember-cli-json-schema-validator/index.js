38220f3ce84f76d8ffe81d87bffffad1d143a319
Randall Morey <rmorey@anypresence.com>
2017-01-18 11:12:17 -0500

    version syntax

--- blueprints/ember-cli-json-schema-validator/index.js
+++ blueprints/ember-cli-json-schema-validator/index.js
@@ -2,6 +2,6 @@ module.exports = {
   description: 'Adds JJV Bower package to project',
   normalizeEntityName: function() {},
   afterInstall: function() {
-    return this.addBowerPackageToProject('jjv#1.0.2');
+    return this.addBowerPackageToProject('jjv', '1.0.2');
   }
 };
